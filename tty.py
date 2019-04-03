465723631d9ad1f3af3bb7a43b48725945051e16
Patrik Černoch <patcernoch@gmail.com>
2019-04-03 11:23:24 +0200

    Add files via upload

--- tty.py
+++ tty.py
@@ -2,6 +2,7 @@ import urllib2
 import urllib
 import json
 import os
+from datetime import datetime
 from flask import render_template, Flask,jsonify,make_response, Response
 os.environ['no_proxy']='*'
 
@@ -14,10 +15,14 @@ def wpjson():
     return data
 
 @app.route('/')
+
 def index():
     data = wpjson()
+    datum = float(data['datum'])
+    data['datum'] = datetime.utcfromtimestamp(datum).strftime('Datum:%Y-%m-%d-Cas:%H:%M:%S')
     return render_template('index.html',data=data)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT',5000))
 app.run(host='0.0.0.0',port = port)
+
